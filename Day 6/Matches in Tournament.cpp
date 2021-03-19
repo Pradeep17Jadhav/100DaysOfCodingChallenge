221f5e098fab437862ae9f720554fcd4a2180dc5
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2021-03-19 16:44:14 +0530

    Create Matches in Tournament.cpp

---
+++ Day 6/Matches in Tournament.cpp
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/count-of-matches-in-tournament
+
+*/
+
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int matchCount = 0;
+        while(n > 1)
+        {
+            if(n % 2 == 0)
+            {
+                matchCount += n / 2;
+                n /= 2;
+            }
+            else
+            {
+                matchCount += (n - 1) / 2;
+                n = (n - 1) / 2 + 1;
+            }
+        }
+        return matchCount;
+    }
+};
